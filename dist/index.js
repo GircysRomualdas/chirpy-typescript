e43e2fdc7a69ec8d88c9821ecbf263c4def20666
romas <r2udis@gmail.com>
2025-08-05 18:44:23 +0300

    add log response middleware

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./middlerware/log_responses.js";
 const app = express();
 const PORT = 8080;
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ src/middlerware/log_responses.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+export async function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  res.on("finish", () => {
+    const statusCode = res.statusCode;
+    if (statusCode > 299) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+    }
+  });
+  next();
+}
