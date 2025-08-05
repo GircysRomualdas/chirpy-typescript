a28582d1b4caed06c15b7320b66f197d40cd9585
romas <r2udis@gmail.com>
2025-08-05 17:50:32 +0300

    add logo image

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 
 const app = express();
-const port = 8080;
+const PORT = 8080;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
