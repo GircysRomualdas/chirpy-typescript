eca46d70338010622e4df2227950066d05c4795b
romas <r2udis@gmail.com>
2025-08-10 01:57:17 +0300

    add JWT token

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { UserNotAuthorizedError } from "./errors.js";
 
 export async function hashPassword(password: string): Promise<string> {
   const saltRounds = 10;
@@ -12,4 +14,43 @@ export async function checkPasswordHash(
 ): Promise<boolean> {
   const isMatch = await bcrypt.compare(password, hash);
   return isMatch;
+}
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+const TOKEN_ISSUER = "chirpy";
+
+export function makeJWT(userID: string, expiresIn: number, secret: string) {
+  const issuedAt = Math.floor(Date.now() / 1000);
+  const expiresAt = issuedAt + expiresIn;
+  const token = jwt.sign(
+    {
+      iss: TOKEN_ISSUER,
+      sub: userID,
+      iat: issuedAt,
+      exp: expiresAt,
+    } satisfies payload,
+    secret,
+    { algorithm: "HS256" },
+  );
+
+  return token;
+}
+
+export function validateJWT(tokenString: string, secret: string) {
+  let decoded: payload;
+  try {
+    decoded = jwt.verify(tokenString, secret) as JwtPayload;
+  } catch (e) {
+    throw new UserNotAuthorizedError("Invalid token");
+  }
+
+  if (decoded.iss !== TOKEN_ISSUER) {
+    throw new UserNotAuthorizedError("Invalid issuer");
+  }
+
+  if (!decoded.sub) {
+    throw new UserNotAuthorizedError("No user ID in token");
+  }
+
+  return decoded.sub;
 }
