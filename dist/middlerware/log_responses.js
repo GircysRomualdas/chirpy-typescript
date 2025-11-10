a5385c87bd7bf00fe97c9d51ca5aac1064243798
romas <r2udis@gmail.com>
2025-11-10 20:33:39 +0200

    remove dist

--- dist/middlerware/log_responses.js
+++
@@ -1,9 +0,0 @@
-export async function middlewareLogResponses(req, res, next) {
-    res.on("finish", () => {
-        const statusCode = res.statusCode;
-        if (statusCode > 299) {
-            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
-        }
-    });
-    next();
-}
--- dist/middleware/metrics.js
+++
@@ -1,5 +0,0 @@
-import { config } from "../config.js";
-export function middlewareMetricsInc(req, res, next) {
-    config.api.fileServerHits++;
-    next();
-}
